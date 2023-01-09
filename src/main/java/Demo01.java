32993628ac494b3f88d1b6a00003ed7fedfbc513
yaogebana <yaogestring@gmail.com>
2023-01-09 18:44:18 +0800

    first commit

---
+++ src/main/java/Demo01.java
@@ -0,0 +1,11 @@
+public class Demo01 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3};
+        int length = arr.length;
+        int index1;
+              for (int i : arr) {
+            for (int i1 = i; i1 > 0; i1--) {
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/demo/gitT.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class gitT {
+    public static void main(String[] args) {
+        System.out.println("提交到暂存区");
+    }
+}
